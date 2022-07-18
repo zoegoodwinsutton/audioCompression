3aaf03f67a39a0da88a29932f7974d7847820510
zoegoodwinsutton <zoe.goodwinsutton@gmail.com>
2022-07-18 13:58:02 -0700

    f

--- example.c
+++ example.c
@@ -8,7 +8,7 @@ char *filename;
 struct HEADER header;
 unsigned char buffer4[4];
 unsigned char buffer2[2];
-
+char* seconds_to_time(float seconds);
 int main(){
     ptr = fopen("zoe.wav", "rb");
     if(ptr == NULL){
@@ -31,15 +31,15 @@ int main(){
     read = fread(header.fmt_chunk_marker, sizeof(header.fmt_chunk_marker), 1, ptr);
     printf("(13-16) Fmt marker: %s\n", header.fmt_chunk_marker);
     read = fread(buffer4, sizeof(buffer4), 1, ptr);
-    printf("%u %u %u %un", buffer4[0], buffer4[1], buffer4[2], buffer4[3]);
+    printf("%u %u %u %u\n", buffer4[0], buffer4[1], buffer4[2], buffer4[3]);
 
  // convert little endian to big endian 4 byte integer
     header.length_of_fmt = buffer4[0] |
                             (buffer4[1] << 8) |
                             (buffer4[2] << 16) |
                             (buffer4[3] << 24);
-    printf("(17-20) Length of Fmt header: %u n", header.length_of_fmt);
-    read = fread(buffer2, sizeof(buffer2), 1, ptr); printf("%u %u n", buffer2[0], buffer2[1]);
+    printf("(17-20) Length of Fmt header: %u \n", header.length_of_fmt);
+    read = fread(buffer2, sizeof(buffer2), 1, ptr); printf("%u %u \n", buffer2[0], buffer2[1]);
     header.format_type = buffer2[0] | (buffer2[1] << 8);
 
     char format_name[10] = "";
@@ -49,56 +49,56 @@ int main(){
         strcpy(format_name, "A-law");
     else if (header.format_type == 7)
         strcpy(format_name, "Mu-law");
-    printf("(21-22) Format type: %u %s n", header.format_type, format_name);
+    printf("(21-22) Format type: %u %s \n", header.format_type, format_name);
     read = fread(buffer2, sizeof(buffer2), 1, ptr);
-    printf("%u %u n", buffer2[0], buffer2[1]);
+    printf("%u %u \n", buffer2[0], buffer2[1]);
     header.channels = buffer2[0] | (buffer2[1] << 8);
-    printf("(23-24) Channels: %u n", header.channels);
+    printf("(23-24) Channels: %u \n", header.channels);
     read = fread(buffer4, sizeof(buffer4), 1, ptr);
-    printf("%u %u %u %un", buffer4[0], buffer4[1], buffer4[2], buffer4[3]);
+    printf("%u %u %u %u\n", buffer4[0], buffer4[1], buffer4[2], buffer4[3]);
     header.sample_rate = buffer4[0] |
                         (buffer4[1] << 8) |
                         (buffer4[2] << 16) |
                         (buffer4[3] << 24);
-    printf("(25-28) Sample rate: %un", header.sample_rate);
+    printf("(25-28) Sample rate: %u\n", header.sample_rate);
     read = fread(buffer4, sizeof(buffer4), 1, ptr);
-    printf("%u %u %u %un", buffer4[0], buffer4[1], buffer4[2], buffer4[3]);
+    printf("%u %u %u %u\n", buffer4[0], buffer4[1], buffer4[2], buffer4[3]);
     header.byterate  = buffer4[0] |
                         (buffer4[1] << 8) |
                         (buffer4[2] << 16) |
                         (buffer4[3] << 24);
-    printf("(29-32) Byte Rate: %u , Bit Rate:%un", header.byterate, header.byterate*8);
+    printf("(29-32) Byte Rate: %u , Bit Rate:%u\n", header.byterate, header.byterate*8);
 
     read = fread(buffer2, sizeof(buffer2), 1, ptr);
-    printf("%u %u n", buffer2[0], buffer2[1]);
+    printf("%u %u \n", buffer2[0], buffer2[1]);
     header.block_align = buffer2[0] |
                     (buffer2[1] << 8);
-    printf("(33-34) Block Alignment: %u n", header.block_align);
+    printf("(33-34) Block Alignment: %u \n", header.block_align);
     read = fread(buffer2, sizeof(buffer2), 1, ptr);
-    printf("%u %u n", buffer2[0], buffer2[1]);
+    printf("%u %u \n", buffer2[0], buffer2[1]);
     header.bits_per_sample = buffer2[0] |
                     (buffer2[1] << 8);
-    printf("(35-36) Bits per sample: %u n", header.bits_per_sample);
+    printf("(35-36) Bits per sample: %u \n", header.bits_per_sample);
     read = fread(header.data_chunk_header, sizeof(header.data_chunk_header), 1, ptr);
-    printf("(37-40) Data Marker: %s n", header.data_chunk_header);
+    printf("(37-40) Data Marker: %s \n", header.data_chunk_header);
     read = fread(buffer4, sizeof(buffer4), 1, ptr);
-    printf("%u %u %u %un", buffer4[0], buffer4[1], buffer4[2], buffer4[3]);
+    printf("%u %u %u %u\n", buffer4[0], buffer4[1], buffer4[2], buffer4[3]);
     header.data_size = buffer4[0] |
                     (buffer4[1] << 8) |
                     (buffer4[2] << 16) |
                     (buffer4[3] << 24 );
-    printf("(41-44) Size of data chunk: %u n", header.data_size);
+    printf("(41-44) Size of data chunk: %u \n", header.data_size);
     // calculate no.of samples
     long num_samples = (8 * header.data_size) / (header.channels * header.bits_per_sample);
-    printf("Number of samples:%lu n", num_samples);
+    printf("Number of samples:%lu \n", num_samples);
 
     long size_of_each_sample = (header.channels * header.bits_per_sample) / 8;
-    printf("Size of each sample:%ld bytesn", size_of_each_sample);
+    printf("Size of each sample:%ld bytes\n", size_of_each_sample);
 
  // calculate duration of file
 
     float duration_in_seconds = (float) header.overall_size / header.byterate;
-    printf("Approx.Duration in seconds=%fn", duration_in_seconds);
-    printf("Approx.Duration in h:m:s=%sn", seconds_to_time(duration_in_seconds));
+    printf("Approx.Duration in seconds=%f\n", duration_in_seconds);
+    printf("Approx.Duration in h:m:s=%s\n", seconds_to_time(duration_in_seconds));
     printf("hello world\n");
 }
