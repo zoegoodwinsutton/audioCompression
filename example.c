31a4ad6b431bb64719360808acdff1f214cacb94
zoegoodwinsutton <zoe.goodwinsutton@gmail.com>
2022-07-20 14:25:34 -0700

    g

--- example.c
+++ example.c
@@ -156,11 +156,11 @@ void readWaveFileSamples(FILE *ptr){
 void writeWaveFileSamples(FILE* outfile){
     int i ;
     // long size_of_each_sample = (header.channels * header.bits_per_sample) / 8;
-    for(i =0; i < num_samples; i++){
-        // buffer2[0] = sample_data[i] & 0x000000FF;
-        // buffer2[1] = (sample_data[i] & 0X0000FF00) >> 8;
-        // fwrite(buffer2,size_of_each_sample,1,outfile);
-    }
+    // for(i =0; i < num_samples; i++){
+    //     // buffer2[0] = sample_data[i] & 0x000000FF;
+    //     // buffer2[1] = (sample_data[i] & 0X0000FF00) >> 8;
+    //     // fwrite(buffer2,size_of_each_sample,1,outfile);
+    // }
     fclose(outfile);
 }
 int signum( int sample) {
