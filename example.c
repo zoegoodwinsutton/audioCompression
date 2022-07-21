6ae132200a7f1ba5dcad4313d8336bf348d741ba
zoegoodwinsutton <zoe.goodwinsutton@gmail.com>
2022-07-20 22:42:09 -0700

    f

--- example.c
+++ example.c
@@ -38,8 +38,8 @@ int main(){
     for(j = 0; j < num_samples; j++){
         printf("%d ", sample_data[j]);
     }
-    printf("Writing WAV file\n");
-    outfile = fopen("output.wav", "w");
+    printf("\nWriting WAV file\n");
+    outfile = fopen("output.txt", "w");
     if(outfile == NULL){
         printf("Unable to open file.\n");
         exit(1);
